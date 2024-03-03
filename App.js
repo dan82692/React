ff3232eabad544893e260886c245c977a43eb4b2
dan82692 <danishraza72@gmail.com>
2024-03-04 05:11:47 +0530

    learning

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import  ReactDOM from "react-dom/client";
+
+
 const parent = React.createElement("div",{id: "parent"},
 React.createElement("div",{id: "child"},
-[React.createElement("h1",{}, "I am h1 Tag"),
+[React.createElement("h1",{}, "I am h1 Tag 🤞😍"),
 React.createElement("h2",{}, "I am h2 Tag")]),
 
 //For making two childs
