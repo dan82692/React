846887980210a612895ae120adb349d271b8129d
dan82692 <danishraza72@gmail.com>
2024-03-05 05:32:06 +0530

    third Day

--- App.js
+++ App.js
@@ -2,18 +2,100 @@ import React from "react";
 import  ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement("div",{id: "parent"},
-React.createElement("div",{id: "child"},
-[React.createElement("h1",{}, "I am h1 Tag 🤞😍"),
-React.createElement("h2",{}, "I am h2 Tag")]),
+// const heading = React.createElement("h1",
+//  { id: "heading"},
+//  "Namaste React🚀🚀"
+//  );
 
-//For making two childs
+// console.log (heading);
 
-React.createElement("div",{id: "child2"},
-[React.createElement("h3",{}, "I am h1 Tag"),
-React.createElement("h4",{}, "I am h2 Tag")])
 
-);
+const Title =()=> (
+    
+<h1 className="head" tabIndex="5">
+    welcome to namaste react🚀🚀
+
+</h1>
+
+    );
+
+const HeadingComponent = () => (
+    <div id = "container">
+        <Title/>
+        <Title></Title>
+        {Title()}
+
+<h1 className="heading"> Namaste React function component
+ </h1>
+ </div>
+ );
+
+
+//This is also valid
+
+
+  
+
+// console.log(jsxheading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(<HeadingComponent/>);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const parent = React.createElement("div",{id: "parent"},
+// React.createElement("div",{id: "child"},
+// [React.createElement("h1",{}, "I am h1 Tag 🤞😍"),
+// React.createElement("h2",{}, "I am h2 Tag")]),
+
+// //For making two childs
+
+// React.createElement("div",{id: "child2"},
+// [React.createElement("h3",{}, "I am h1 Tag"),
+// React.createElement("h4",{}, "I am h2 Tag")])
+
+// );
 
 
 // const heading = React.createElement
@@ -21,8 +103,8 @@ React.createElement("h4",{}, "I am h2 Tag")])
 // {id: "heading"}, 
 // "Hello world from react");
 
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading);
-root.render(parent);
+// // root.render(heading);
+// root.render(parent);
