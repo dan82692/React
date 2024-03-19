aad4fcaac87ee9580913b3edebd95bc6325d08d6
dan82692 <danishraza72@gmail.com>
2024-03-19 06:47:16 +0530

    updated

--- App.js
+++ App.js
@@ -10,28 +10,72 @@ import  ReactDOM from "react-dom/client";
 // console.log (heading);
 
 
-const Title =()=> (
-    
-<h1 className="head" tabIndex="5">
-    welcome to namaste react🚀🚀
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="Logo-container">
+            <img className="Logo" src="https://i.pinimg.com/originals/3c/b2/4f/3cb24f7d0defcb91eb51b091325d5f47.jpg"/>
+            
+            </div>
+            <div className="pop">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
 
-</h1>
-
+        </div>
     );
 
-const HeadingComponent = () => (
-    <div id = "container">
-        <Title/>
-        <Title></Title>
-        {Title()}
+};
 
-<h1 className="heading"> Namaste React function component
- </h1>
- </div>
- );
+const styleCard = {
+    backgroundColor : "#f0f0f0",
+};
 
 
-//This is also valid
+const ResturantCard = ({resName, cuisine,Rating , time , imageurl }) => {
+    return(
+<div className="res-card" style={styleCard}>
+   
+ <img className="res-image" alt="res-image" src={imageurl}></img>
+
+<h3>{resName}</h3>
+<h4>{cuisine}</h4>
+<h4>{Rating}</h4>
+<h4>{time}</h4>
+</div>
+    );
+
+};
+
+const Body = () => {
+    return(
+
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <ResturantCard resName="Red Chilli" cuisine="Chowmin , Chinese" Rating="3.5" time="30 Minutes" imageurl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3WcgGWLYXzyCf-gzIQI-Hf7ndsdgrcm_TtQ&s"/>
+                <ResturantCard resName="KFC" cuisine="Burger, Fast food" Rating="3.9" time="20 Minutes" imageurl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8jqbhz44--Bxz4ZcuugSUoVRrIusoVmCB2Q&s"/>
+                <ResturantCard resName="Dominos" cuisine="Pizza , Fries" Rating="3.8" time="25 Minutes" imageurl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUDMhPpndCvGlsF_NF-j1olBZVn9Gg8QDerw&s"/>
+                
+            </div>
+        </div>
+    );
+};
+
+const Foodapp = () => {
+    return(
+    
+        <div className="Food">
+            <Header/>
+            <Body/>
+
+        </div>
+    );
+};
 
 
   
@@ -40,7 +84,7 @@ const HeadingComponent = () => (
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
-root.render(<HeadingComponent/>);
+root.render(<Foodapp/>);
 
 
 
